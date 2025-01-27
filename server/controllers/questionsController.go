b73e920b8f0166ce31272187294ac344bc7c7b38
om-baji <ombaji124@gmail.com>
2025-01-27 11:38:49 +0530

    Gemini ques controller

--- server/controllers/questionsController.go
+++ server/controllers/questionsController.go
@@ -2,15 +2,17 @@ package controllers
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/google/generative-ai-go/genai"
+	"github.com/om-baji/models"
 	"google.golang.org/api/option"
 )
 
-func getQuestion(limit int64, tag string) (*genai.GenerateContentResponse, error) {
+func getResponse(limit int64, tag string) (*genai.GenerateContentResponse, error) {
 	api_key := os.Getenv("GEMINI_API_KEY")
 
 	ctx := context.Background()
@@ -22,7 +24,10 @@ func getQuestion(limit int64, tag string) (*genai.GenerateContentResponse, error
 	}
 
 	prompt := fmt.Sprintf(
-		"Give %d MCQ question(s) on advanced, high-level, and core %s. It should return an array of objects with fields for options, question, correctAnswer, and tag with the subject name matching my input. Avoid repeated questions.",
+		"Give %d MCQ question(s) on advanced, high-level, and core %s. "+
+			"Questions should be incredibly hard testing one's core knowledge and fundamentals at its finest. "+
+			"It should return an array of objects with fields for options, question, correctAnswer, and tag with the subject name matching my input. "+
+			"Avoid repeated questions. It should only have a json response not anything other than that",
 		limit, tag)
 
 	model := client.GenerativeModel("gemini-1.5-flash")
@@ -37,4 +42,23 @@ func getQuestion(limit int64, tag string) (*genai.GenerateContentResponse, error
 
 	return response, nil
 
+}
+
+func getQuestion(limit int64, tag string) []models.Question {
+
+	var questions []models.Question
+
+	response, err := getResponse(limit, tag)
+
+	if err != nil {
+		log.Fatal("Something went wrong!")
+	}
+
+	err = json.Unmarshal([]byte(response.PromptFeedback.BlockReason.String()), &questions)
+
+	if err != nil {
+		fmt.Printf("Something went wrong!\n %v\n", err.Error())
+	}
+
+	return questions
 }
